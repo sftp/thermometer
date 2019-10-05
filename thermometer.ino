fdd230abcf333c8dee54199c618edd33e263af4e
Sergey V <sftp.mtuci@gmail.com>
2019-10-05 19:36:47 +0300

    move find_t() to tc.h

--- thermometer.ino
+++ thermometer.ino
@@ -50,46 +50,6 @@ uint16_t adc;
 int16_t t;
 int32_t ds18b20_t;
 
-uint16_t find_t(uint16_t x)
-{
-  uint16_t a;
-  uint16_t b;
-  uint16_t left;
-  uint16_t right;
-  uint16_t pos;
-  uint16_t ret;
-
-  pos = x / TABLE_STEP;
-
-  if (pos >= TABLE_SIZE) {
-    return pgm_read_word_near(TABLE + TABLE_SIZE);
-  }
-
-  a = pgm_read_word_near(TABLE + pos);
-  b = pgm_read_word_near(TABLE + pos + 1);
-  left = pos * TABLE_STEP;
-  right = (pos + 1) * TABLE_STEP;
-
-  ret = a + ((x - left) * (b - a)) / (right - left);
-
-#if DEBUG == 1
-  Serial.print("adc = ");
-  Serial.print(x);
-  Serial.print("\ta = ");
-  Serial.print(a);
-  Serial.print("\tb = ");
-  Serial.print(b);
-  Serial.print("\tleft = ");
-  Serial.print(left);
-  Serial.print("\tright = ");
-  Serial.print(right);
-  Serial.print("\tt = ");
-  Serial.println(ret / 10.0);
-#endif
-
-  return ret;
-}
-
 void setup() {
 
 #if DEBUG == 1
